bd6add723087aa2e37f090e28faf41808d613d56
jamesr <james.richardson@cdl.co.uk>
2013-07-01 11:06:50 +0100

    Adding slide 1 of talk

---
+++ src/monadic/Slide1.scala
@@ -0,0 +1,47 @@
+package monadic
+
+object Slide1 {
+
+  val lastNames = Map("1" -> "Richardson", "2" -> "Robinson").withDefaultValue(null)
+  
+  val firstNames = Map("1" -> "James", "2" -> "Simon").withDefaultValue(null)
+  
+  def lastName(id: String): String = lastNames(id)
+  
+  def firstName(id: String): String = firstNames(id)
+  
+  def fullName(id: String): String = {
+    val last = lastName(id)
+    
+    if (last != null) {
+      val first = firstName(id)
+      
+      if (first != null) {
+        first + " " + last
+      } else {
+        null
+      }
+    } else {
+      null
+    }
+  }
+  
+  def main(args: Array[String]): Unit = {
+    println("1 -> " + fullName("1"))
+    println("2 -> " + fullName("2"))
+    println("3 -> " + fullName("3"))
+  }
+}
+
+
+trait Maybe[T] {
+  
+}
+
+object Nothing extends Maybe[Any] {
+  
+}
+
+class Some[T](val value: T) extends Maybe[T] {
+  
+}
