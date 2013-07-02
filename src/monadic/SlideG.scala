32240cbf79994beb2b5d9fc59af07df331f4d8bf
jamesr <james.richardson@cdl.co.uk>
2013-07-02 11:29:08 +0100

    Adding slides on how to generalise into arrows

---
+++ src/monadic/SlideG.scala
@@ -0,0 +1,26 @@
+package monadic
+
+/**
+ * Generalising Monads into Arrows
+ */
+object SlideG {
+
+  def currentAge(year: Int):Int = year - 1977
+  
+  def convertToString(x: Int):String = "" + x
+  
+  def prettyPrint(s: String):String = s + "yrs"
+  
+  def printIt(s: String):Int = {println(s); s.length } 
+  
+  def main(args: Array[String]):Unit = {
+    printIt(prettyPrint(convertToString(currentAge(2013))))
+  }
+  
+  // can expand functions
+  class Arrow[A, B](f: A => B) {
+    def apply(a: A): B = f(a)
+    
+    // def >>>[C](otherArrow: Arrow[B, C]) = ?
+  }
+}
---
+++ src/monadic/SlideH.scala
@@ -0,0 +1,42 @@
+package monadic
+
+/**
+ * Logging trace with Arrows
+ */
+object SlideH {
+
+  // An Arrow
+  class LoggingArrow[A, B](val name: String, arrow: A => B) {
+    def apply(a: A): B = {
+      // a simple log message
+      println(name + "(" + a + ")")
+      // do the action
+      arrow(a)
+    }
+    
+    def >>>[C](otherArrow: LoggingArrow[B, C]) = {
+      val name = otherArrow.name + "(" + this.name + "(_))"
+      new LoggingArrow[A, C](name, a => otherArrow(arrow(a)))
+    }
+  }
+  
+  def arr[X, Y](name: String, arrow: X => Y) = new LoggingArrow[X, Y](name, arrow)
+  
+  // Previous problem becomes
+  val currentAge = arr[Int, Int]("currentAge", year => year - 1977)
+  
+  val convertToString = arr[Int, String]("convertToString", x => "" + x)
+  
+  val prettyPrint = arr[String, String] ("prettyPrint", s => s + "yrs")
+  
+  val printIt = arr[String, Int]("printIt", s => {println(s); s.length})
+  
+  def main(args: Array[String]):Unit = {
+    // build our action
+    val action = currentAge >>> convertToString >>> prettyPrint >>> printIt
+    // run our action
+    action(2013)
+    
+    // What about if we also log the function result?
+  }
+}
